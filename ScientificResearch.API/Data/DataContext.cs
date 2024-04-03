c3f3b615fae0084523159be236c7930f103e4328
Laura Posada <lauraposada0402@gmail.com>
2024-04-03 17:07:29 -0500

    DataContext class was created

---
+++ ScientificResearch.API/Data/DataContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Reflection.Emit;
+
+namespace ScientificResearch.API.Data
+{
+    public class DataContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ScientificInvestigation> ScientificInvestigations { get; set; }
+        public DbSet<Investigador> Investigadors { get; set; }
+        public DbSet<searchActivity> searchActivities { get; set; }
+
+        public DbSet<specializedResource> specializedResources { get; set; }
+
+        public DbSet<Publication> Publications { get; set; }
+
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
+
