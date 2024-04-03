5e1fb9fff1ee1b0f523a46e5d4f3bb17297939b3
Laura Posada <lauraposada0402@gmail.com>
2024-04-03 17:31:00 -0500

    Updated Datacontext class

--- ScientificResearch.API/Data/DataContext.cs
+++ ScientificResearch.API/Data/DataContext.cs
@@ -12,16 +12,16 @@ namespace ScientificResearch.API.Data
         {
         }
 
-        public DbSet<ScientificInvestigation> ScientificInvestigations { get; set; }
+        public DbSet<Publication> Publications { get; set; }
+
         public DbSet<Investigador> Investigadors { get; set; }
+
         public DbSet<searchActivity> searchActivities { get; set; }
 
         public DbSet<specializedResource> specializedResources { get; set; }
 
-        public DbSet<Publication> Publications { get; set; }
-
-
-
+        public DbSet<ScientificInvestigation> ScientificInvestigations { get; set; }
+  
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
